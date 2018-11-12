7fc4fc035ca076963e07e55f424b9b4acd6ea711
v1k45 <v1k45x@gmail.com>
2018-11-12 18:02:32 +0530

    Remove placeholder views; Initiate project
    - Add bulma css to dependency
    - Setup sample risk type views

---
+++ src/components/RiskTypeItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li :key="item.id" v-for="item in items">
+      {{ item.name }}
+    </li>
+  </ul>
+</template>
+
+
+<script>
+export default {
+  name: 'risk-type-item',
+  props: ['items'],
+};
+</script>
---
+++ src/views/RiskTypeList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="risk-type-list">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Risk Types</h1>
+          <h2 class="subtite">Select a risk type to get started.</h2>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <risk-type-item :items="riskTypes"></risk-type-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import RiskTypeItem from '@/components/RiskTypeItem.vue';
+
+export default {
+  name: 'risk-type-list',
+  components: {
+    RiskTypeItem,
+  },
+  computed: {
+    riskTypes() {
+      return [
+        { id: 1, name: 'Cars' },
+        { id: 2, name: 'Lottery' },
+      ];
+    },
+  },
+  methods: {
+  },
+  created() {
+    document.title = 'Risk Types';
+  },
+};
+</script>
